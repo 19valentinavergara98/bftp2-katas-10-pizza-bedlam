c24235295b5360ac83e7544a7317fa9204a150c0
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-02-22 10:22:33 +0100

    creando class product y test

--- src/test/java/PizzaBedlamTest.java
+++ src/test/java/PizzaBedlamTest.java
@@ -1,3 +1,13 @@
-public class PizzaBedlamTest {
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
+public class PizzaBedlamTest {
+    @Test
+    void pizzaCostDependsName() {
+        assertThat(new Pizza("MARGHERITA").getPrice(), equalTo(9.30));
+        assertThat(new Pizza("PROSCIUTTO ").getPrice(), equalTo(12.00));
+        assertThat(new Pizza("PROSCIUTTO E FUNGHI").getPrice(), equalTo(12.50));
+        assertThat(new Pizza("4 STAGIONI").getPrice(), equalTo(12.50));
+    }
 }
