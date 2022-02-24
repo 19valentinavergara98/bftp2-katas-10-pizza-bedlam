580afeae721392ad366fbd8920af4a3679ee4449
Marisa Posada <marisa.posada@gmail.com>
2022-02-24 09:41:37 +0100

    pizzas prices

--- src/main/java/product/Pizza.java
+++ src/main/java/product/Pizza.java
@@ -1,7 +1,28 @@
 package product;
 
-public class Pizza {
-    public Pizza(String margherita) {
+import java.math.BigDecimal;
+
+public class Pizza implements BedlamProducts{
+    private final BigDecimal price;
+    private final String name;
+
+    public Pizza (BigDecimal price, String name) {
+        this.price = price;
+        this.name = name;
     }
+
+    public BigDecimal getPrice() {
+        return switch (name) {
+            case "Margherita" -> price + 9.30;
+            case "Prosciutto" -> price + 12.00;
+            case "Prosciutto e funghi" -> price + 12.50;
+            case "4 Stagioni" -> price + 12.50;
+            default -> price;
+        };
+    }
+
+
+
+
 
 }
